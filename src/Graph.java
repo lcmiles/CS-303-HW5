ea9f53e64b67dac7627046c0f41f6fbc44b376ce
lcmiles <lcmiles@example.com>
2023-10-23 20:40:24 -0500

    Updated documentation

--- src/Graph.java
+++ src/Graph.java
@@ -5,96 +5,110 @@ import java.util.Queue;
 
 public class Graph {
     
-    private int vertices;
-    private int edges;
-    private int adjMatrix[][];
+    private int vertices; //number of vertices in the graph
+    private int edges; //number
     private List<List<Integer>> adjList;
 
 
-    public Graph(int nodes) {
-        this.vertices = nodes;
-        this.edges = 0;
-        this.adjMatrix = new int[nodes][nodes];
-        this.adjList = new ArrayList<>();
-        for (int i = 0; i < nodes; i++) {
-            adjList.add(new ArrayList<>());
+    public Graph(int nodes) { //constructor for Graph object
+        this.vertices = nodes; //initializes the number of vertices as the number of nodes
+        this.edges = 0; //initializes the number of edges to 0
+        this.adjList = new ArrayList<>(); //initilizes ArrayList for adjacency lists for each node
+        for (int i = 0; i < nodes; i++) { //for each node within the graph a new adjacency list is created
+            adjList.add(new ArrayList<>()); //adds the list for each node to adjList
         }
     }
 
-    public void addEdge(int u, int v) {
-        adjMatrix[u][v] = 1;
-        adjMatrix[v][u] = 1;
-        adjList.get(u).add(v);
-        adjList.get(v).add(u);
+    /*
+    Description: This function adds undirected edges to the graph represented as a 1 in the adjacency matrix both ways
+    Parameters:
+    int node1 - A node read from the text file to be connected to node2 via an edge
+    int node2 - A node read from the text file to be connected to node1 via an edge
+    Returns: Nothing
+    */
+    public void addEdge(int node1, int node2) {
+        adjList.get(node1).add(node2); //gets the adjacency list for node1 and adds node2 to that list
+        adjList.get(node2).add(node1); //gets the adjacency list for node2 and adds node1 to that list
         edges++;
     }
 
+    
+    /*
+    Description: This function iterates through the list of adjacency lists for each node, appending each element to a string and printing each of them to form an adjacency list for the graph
+    Parameters: None
+    Returns: Nothing
+    */
     public void printAdjList() {
-        StringBuilder string = new StringBuilder();
-        string.append("The graph contains "+ edges + " edges and " + vertices + " vertices.");
-        for (int i = 0; i < vertices; i++) {
+        StringBuilder string = new StringBuilder(); //initialize StringBuilder object
+        string.append("The graph contains "+ edges + " edges and " + vertices + " vertices. \n"); 
+        for (int i = 0; i < vertices; i++) { //iterates through the list of adjacency lists using using a for loop stopping at the number of vertices
             string.append("Adjacency list for node " + i + ": ");
-            for (int neighbor : adjList.get(i)) {
-                string.append(neighbor + " ");
+            for (int neighbor : adjList.get(i)) { //for each neighbor contained in the nested adjacency list 
+                string.append(neighbor + " "); 
             }
             string.append("\n");
         }
         System.out.println(string);
     }
 
-    public int getVertices() {
+    public int getVertices() { //getter for vertices
 		return this.vertices;
 	}
 
-	public void setVertices(int vertices) {
+	public void setVertices(int vertices) { //setter for vertices
 		this.vertices = vertices;
 	}
 
-	public int getEdges() {
+	public int getEdges() { //getter for edges
 		return this.edges;
 	}
 
-	public void setEdges(int edges) {
+	public void setEdges(int edges) { //setter for edges
 		this.edges = edges;
 	}
 
-	public List<Integer> getAdjList(int node) {
+	public List<Integer> getAdjList(int node) { //getter for adjList of a certain node
         return adjList.get(node);
     }
 
+    /*
+    Description: This function uses breadth-first search to traverse the graph and print each node
+    Parameters: None
+    Returns: Nothing
+    */
     public void BFS(int startNode) {
-        boolean[] visited = new boolean[vertices];
-        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[vertices]; //matrix containing boolean values for each node; 1 means visited, 0 means unvisited
+        Queue<Integer> queue = new LinkedList<>(); //creates a queue of integers using a LinkedList as the underlying data structure to queue neighbors 
 
         visited[startNode] = true;
         queue.add(startNode);
 
         System.out.print("Breadth-First Search starting from node " + startNode + ": ");
-        while (!queue.isEmpty()) {
-            int node = queue.poll();
-            System.out.print(node + " ");
+        while (!queue.isEmpty()) { //while the queue is not empty
+            int node = queue.poll(); //dequeues the front node and assigns it to the node variable
+            System.out.print(node + " "); //prints node to indicate which node is currently being processed
 
-            for (int neighbor : adjList.get(node)) {
-                if (!visited[neighbor]) {
-                    visited[neighbor] = true;
-                    queue.add(neighbor);
+            for (int neighbor : adjList.get(node)) { //iterates through neighbors of the current node 
+                if (!visited[neighbor]) { //if the neighbor has not been visited yet
+                    visited[neighbor] = true; //set visited to true in matrix
+                    queue.add(neighbor); //add neighbor to que for printing
                 }
             }
         }
 
-        // Check if any node remained unvisited
-        boolean hasUnvisitedNodes = false;
-        for (int i = 0; i < vertices; i++) {
-            if (!visited[i]) {
-                hasUnvisitedNodes = true;
+        boolean hasUnvisitedNodes = false; //unvisited nodes flag
+        for (int i = 0; i < vertices; i++) { //iterates through vertices to check if any node remained unvisited
+            if (!visited[i]) { //if the current node is unvisited
+                hasUnvisitedNodes = true; //set flag to true
                 break;
             }
         }
 
-        if (hasUnvisitedNodes) {
+        if (hasUnvisitedNodes) { //case for there being no path to some nodes
             System.out.println("\nThere is no path to some nodes.");
         } else {
-            System.out.println(); // Move to the next line after printing the BFS traversal.
+            System.out.println(); //move to the next line after printing the BFS traversal
         }
+        System.out.println("");
     }
 }
--- src/HW5.java
+++ src/HW5.java
@@ -6,32 +6,44 @@ import java.nio.file.Paths;
 
 public class HW5 {
 
+    /*
+    Description: Executable function that is responsible for reading the text file and calling the functions in the Graph class that build, print, and perform BFS on the graph
+    Parameters:
+    String[] args - Runtime arguments
+    Returns: Nothing
+    */
     public static void main(String[] args) {
-        String filename = "mediumG.txt";
+        String filename = "mediumG.txt"; //initialize filename string variable
         try {
-            long numberOfLines = Files.lines(Paths.get(filename)).count();
-            Graph graph = new Graph((int)numberOfLines); //initialize graph object
+            long numberOfLines = Files.lines(Paths.get(filename)).count(); //count the number of lines in the text file
+            Graph graph = new Graph((int)numberOfLines); //initialize graph object with the number of lines in the text file with the number of nodes
             BufferedReader reader = new BufferedReader(new FileReader(filename)); //initialize reader object
             String line;
             int lineCount = 0; //initialize line count
-            while ((line = reader.readLine()) != null) {
-                lineCount++;
-                if (lineCount == 1) {
-                    graph.setVertices(Integer.parseInt(line));
+            while ((line = reader.readLine()) != null) { //iterate through the text file with a while loop
+                lineCount++; //iterate the line count variable forward 1 with each loop
+                if (lineCount == 1) { //case for first line
+                    graph.setVertices(Integer.parseInt(line)); //parse the int value representing the number of vertices and assign it to the graph object
                 }
-                else if (lineCount == 2) {
-                    graph.setEdges(Integer.parseInt(line));
+                else if (lineCount == 2) { //case for second line
+                    continue; //skip because addEdge() adds edges
                 }
                 else {
-                    String[] nodes = line.split(" ");
-                    int node1 = Integer.parseInt(nodes[0]);
-                    int node2 = Integer.parseInt(nodes[1]);
-                    graph.addEdge(node1, node2);
+                    String[] nodes = line.split(" "); //split each line at the space using a regex
+                    int node1 = Integer.parseInt(nodes[0]); //parse the int value and assign it to node1
+                    int node2 = Integer.parseInt(nodes[1]); //parse the int value and assing it to node2 
+                    graph.addEdge(node1, node2); //call addEdge() on both nodes to add them to the graph
                 }
             }
             reader.close();
-            graph.printAdjList();
-            graph.BFS(169);
+            graph.printAdjList(); //call printAdjList() to print the adjacency list representation of the graph
+            graph.BFS(10); //call BFS on following nodes
+            // graph.BFS(100);
+            // graph.BFS(1000);
+            // graph.BFS(1000);
+            // graph.BFS(10000);
+            // graph.BFS(100000);
+            // graph.BFS(1000000);
         } catch (IOException e) {
             e.printStackTrace();
         }
